10c74eb50e23fd625cb68f4e87d530d3d1a20152
kyuo9944 <67478151+kyuo9944@users.noreply.github.com>
2020-06-30 01:52:36 +0900

    Delete toggleBtn.js

--- toggleBtn.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('.button');
-const list = document.querySelector('.nav_list');
-const icons = document.querySelector('.nav_icons');
-toggleBtn.addEventListener('click', () => {
-    list.classList.toggle('active');
-    icons.classList.toggle('active');
-});
